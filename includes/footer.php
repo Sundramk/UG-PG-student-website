1820a59c6652aa8ec9d9c0b47324a5fe0a79ad5c
Sundramk <sundramkumar21102@gmail.com>
2022-06-17 09:27:41 +0530

    Initial commit

---
+++ includes/footer.php
@@ -0,0 +1,25 @@
+<div class="footer">
+    <div class="page-container footer-container">
+        <div class="footer-cities">
+            <div class="footer-city">
+                <a href="property_list.php?city=Delhi">PG in Delhi</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Mumbai">PG in Mumbai</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Bengaluru">PG in Bangalore</a>
+            </div>
+            <div class="footer-city">
+                <a href="property_list.php?city=Hyderabad">PG in Hyderabad</a>
+            </div>
+        </div>
+        <div class="footer-copyright">© 2020 Copyright PG Life </div>
+    </div>
+</div>
+
+<script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/bootstrap.min.js"></script>
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+<script type="text/javascript" src="js/common.js"></script>
